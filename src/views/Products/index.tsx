963f5fcd6b628ce1fed8581be824f258ed6fd7b4
rababkh22 <rababkhtib2020@gmail.com>
2024-07-27 21:54:14 +0100

    new commit

--- src/views/Products/index.tsx
+++ src/views/Products/index.tsx
@@ -7,6 +7,9 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
 import Grid from '@mui/material/Grid';
+import Chip from '@mui/material/Chip';
+import { styled } from '@mui/material/styles';
+import { Button } from '@mui/material';
 
 interface Product {
   id: number;
@@ -16,6 +19,21 @@ interface Product {
   category: string; 
   images: string[];
 }
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  maxWidth: 340,
+  margin: theme.spacing(2),
+  backgroundColor: '#f0f0f0',
+  transition: 'transform 0.2s',
+  '&:hover': {
+    transform: 'scale(1.05)',
+  },
+}));
+
+
+const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
+  height: 200,
+}));
 
 const Products = (): JSX.Element => {
 
@@ -31,32 +49,35 @@ const Products = (): JSX.Element => {
 
   return (
     <Main>
-      <Box>
-        <Grid container spacing={2}>
+      <Box sx={{ padding: 4 }}>
+        <Grid container spacing={3}>
           {productsData.map((product) => (
             <Grid
               key={product.id}
               item
-              xs={4}
+              xs={12}
+              sm={6}
+              md={4}
             >
-              <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                  sx={{ height: 140 }}
+              <StyledCard>
+                <StyledCardMedia
                   image={product.images[0]}
                   title={product.title}
                 />
                 <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
+                  <Typography gutterBottom variant="h6" color="CaptionText"  component="div">
                     {product.title}
-                  </Typography>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {product.price + ' $'}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
                     {product.description}
                   </Typography>
+                  <Box sx={{ marginTop: 1 , display: 'flex', alignItems: 'center',  gap: 17}}>
+                    <Chip label={product.price + ' $'}  color="default" />
+                    <Button variant="contained" color="primary">Buy</Button>
+                  </Box>
+                 
                 </CardContent>
-              </Card>
+              </StyledCard>
             </Grid>
           ))}
         </Grid>
